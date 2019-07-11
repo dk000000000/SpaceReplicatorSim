703080ed43e26a02837fb5cc3362fe27c53ad350
elmaj-ma <52736197+elmaj-ma@users.noreply.github.com>
2019-07-11 18:48:33 +0200

    Update Agent.py
    Still unfinished .

--- Agent.py
+++ Agent.py
@@ -2,52 +2,81 @@ from copy import copy, deepcopy
 
 class Agent(object):
 
-    systemsBeliefs = [] # list of n star systems each with its coordonates and resource amount
+    systemsBeliefs = [] # list of n star systems each resource amount
 
-    pastPositions = [] #list of index of system they known have been to or other bots have been to
+    pastPositions = [] # list of index of system they known have been to or other bots have been to
 
-    charge = 100 #0-100
+    charge = 100 # charge/Energy Amount -> 0 - 100
 
-    position = 0 #star system index in the universe
+    position = 0 # star system index in the universe
 
-    block = False #boolean if it is blocked
+    block = 0 # boolean if it is blocked
 
-    beliefMatrix = [] # n*n distance between
-
-    dead = False #out of charge
+    dead = False # out of charge
 
     degradation = 0.2
 
+    recharge_speed = 0.4
+
     def _updateCharge(self, ope):
+
       if ope == 0:
-          self.charge *= self.degradation
+
+          self.charge += self.charge * self.recharge_speed
+
       elif ope == 1:
-          self.charge *= self.degradation
+
+          self.charge *= 2 * self.degradation
+
+      elif ope == 2:
+
+          self.charge *= (1 - self.degradation)
+
       else:
+
           self.charge /= 2
+
       if self.charge == 0:
+
           self.dead = 1
 
+
     def _updateBeliefs(self):
+
       self.systemsBeliefs = 0
 
+
     def _updateposition(self, new_position):
+
       self.pastPositions.append(new_position)
+
       self.position = new_position
+
 
     def move(self, new_position):
-      self.pastPositions.append(self.position)
-      self.position = new_position
-      self._updateCharge()
-      #syst.move((self.systemsBeliefs[new_position])[0])
 
-    def getdistanceMatrix(self):
-      return self.distanceMatrix
+      self.pastPositions.append(self.position)
+
+      self.position = new_position
+
+      block = 1
+
+      self._updateCharge(1)
 
     def replicate(self):
+
       self._updateCharge(3)
+
       ag = copy(self)
+
+      block = 1
+
       return ag
 
-    def stay(self, index):
+    def recharge(self):
+
       self._updateCharge(0)
+
+    def stay(self, index):
+
+      self._updateCharge(2)
