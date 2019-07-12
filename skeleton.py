cc81dedd5633bd73d89a7be8ff8c1eaff1d302ac
Simon Mandlik <simon.mandlik@gmail.com>
2019-07-12 13:20:36 +0200

    ...

--- skeleton.py
+++ skeleton.py
@@ -1,25 +1,25 @@
 from Galaxy import Galaxy
 from Probe import Probe
 from random import choice
+import numpy as np
 
 g = Galaxy()
 
 ACTION_LIST = ["recharge", "move", "stay", "replicate"]
 
-def n_closest_planets_absolute(p, g, N):
+def n_closest_planets_absolute(p, g, N=5):
     # get Systems
-    s = g.systemlist
-    curr_id = p.current_position
-    distance = np.asarray(p.getdistanceMatrix()[curr_id, :])
-    index = np.argsort(distance)[1:(n+1)]
+    curr_id = p.galaxy_dict[p.current_position]
+    distance = np.asarray(g.distanceMatrix[curr_id, :])
+    index = np.argsort(distance)[1:(N+1)]
     return index
 
-def decide(probe):
-    a = random.choice(ACTION_LIST)
+def decide(p, g):
+    a = choice(ACTION_LIST)
     a = "move"
     if a == "move":
-        planets = n_closest_planets_absolute(p)
-        return a, random.choice(planets).position
+        planets = n_closest_planets_absolute(p, g)
+        return a, choice(planets)
     else:
         return a, 0
 
@@ -27,5 +27,15 @@ def step(g):
     pl = g.probelist
     actionD = {}
     for k, p in pl.items():
-        actionD[k] = decide(p)
+        actionD[k] = decide(p, g)
+    print(actionD)
     g.step(actionD)
+
+print(g.probelist)
+print(g.probelist['0'].current_position)
+step(g)
+print(g.probelist)
+print(g.probelist['0'].current_position)
+step(g)
+print(g.probelist)
+print(g.probelist['0'].current_position)
