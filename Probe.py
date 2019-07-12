a36a2a4f07c80852f1c3fc0ff854bfad43e0412b
Jieming Ji <jiemingji1996@gmail.com>
2019-07-12 15:11:41 +0200

    fix move in probe

--- Probe.py
+++ Probe.py
@@ -1,6 +1,7 @@
 from copy import copy, deepcopy
 import math
 import numpy as np
+from scipy.spatial.distance import euclidean
 
 class Probe(object):
     def __init__(self,id,galaxy_positions,system,maxcharge,galaxy_dict,dist_matrix,charge=100,degradation = 0.01,recharge_speed=0.2,move_stay_ratio=2,moving_speed=0.01):
@@ -13,7 +14,7 @@ class Probe(object):
         # dict of list of position of system represented three number in the cartesian plane they known have been to or other bots have been to
         self.positions = {self.id:[system.position]} #{"1":[(0,0,0)]}
         # dict of n star systems each resource amount and how much the probe belives about the certainty of resource
-        self.system_beliefs = {p:[maxcharge*np.random.normal(self._getDistance(p)),0] for p in galaxy_positions} #{(0.2,0.2,0.2):[20000,0.8]} (0.2,0.2,0.2) position of the system 20000 is quantity of resource, 0.8 is the certainty of the beliefs
+        self.system_beliefs = {p:[maxcharge*np.random.normal(euclidean(p,(0,0,0))),0] for p in galaxy_positions} #{(0.2,0.2,0.2):[20000,0.8]} (0.2,0.2,0.2) position of the system 20000 is quantity of resource, 0.8 is the certainty of the beliefs
         self.system_beliefs[system.position]=[system.charge,1]#absolutely sure about current system cahrge
         self.galaxy_positions = galaxy_positions
         self.dist_matrix = dist_matrix
@@ -27,11 +28,9 @@ class Probe(object):
         self.system = system
         self.system.movein(self.id)
 
-    def _getDistance(self,position1,position2=(0,0,0)):
-        return np.linalg.norm(np.array(position1)-np.array(position2))
 
     def _getDirectionVector(self,position2):
-        return (np.array(position2)-np.array(self.current_position))/self._getDistance(self.current_position,position2)
+        return (np.array(position2)-np.array(self.current_position))/euclidean(self.current_position,position2)
 
     def _updateCharge(self, act):
         if act == "recharge": #increase the different between recharge and degreadation
@@ -69,24 +68,36 @@ class Probe(object):
         if self.block ==-1:
             if new_position!=self.current_position:
                 self.destination = new_position
-                distance = self._getDistance(self.current_position,self.destination)
-                self.block = math.ceil(distance/self.moving_speed)-2
-                self.blockAction = "move"
-                self.current_position = tuple(np.array(self.current_position) + self.moving_speed*self._getDirectionVector(self.destination))
-                self.system.moveout(self.id)
+                distance = euclidean(self.current_position,self.destination)
+                if distance <=self.moving_speed:
+                    self._helpMove()
+                    return self.current_position
+                else:
+                    self.block = math.ceil(distance/self.moving_speed)-1
+                    self.blockAction = "move"
+                    self.current_position = tuple(np.array(self.current_position) + self.moving_speed*self._getDirectionVector(self.destination))
+                    self.system.moveout(self.id)
             else:
                 self.stay()
         elif self.block == 0:
-            self.current_position = self.destination
-            self.destination =None
-            self.block=-1
-            self.blockAction = None
+            self._helpMove()
             return self.current_position
         else:
-            self.current_position = tuple(np.array(self.current_position) + self.moving_speed*self._getDirectionVector(self.destination))
-            self.block-=1
+            distance = euclidean(self.current_position,self.destination)
+            if distance <=self.moving_speed:
+                self._helpMove()
+                return self.current_position
+            else:
+                self.current_position = tuple(np.array(self.current_position) + self.moving_speed*self._getDirectionVector(self.destination))
+                self.block-=1
 
         self._updateCharge("move")
+
+    def _helpMove(self):
+        self.current_position = self.destination
+        self.destination =None
+        self.block=-1
+        self.blockAction = None
 
 
     def replicate(self):
